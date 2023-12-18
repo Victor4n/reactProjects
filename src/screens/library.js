f93ce040ba5388e354964b8bf010d83a5bc91470
Victor <v1ctor4nt15@gmail.com>
2023-12-18 03:28:58 -0500

    Styles and library modify

--- src/screens/library.js
+++ src/screens/library.js
@@ -1,5 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import APIKit from "../spotify";
+import "./library.css";
 
 export default function Library() {
-  return  <div className='screen-container'></div>;
+  
+  const [playlists, setPlayLists] = useState(null);
+
+  useEffect(() => {
+    APIKit.get('me/playlists').then(function(response) {
+    setPlayLists(response.data.items);
+    console.log(response.data.items);
+  });
+  }, []);
+  return (
+    <div className="screen-container">
+        <div className='library-body'>
+            {playlists?.map((playlist) => (
+                <div className='playlist-card'>
+                    <img src={playlist.images[0].url} className='playlist-image' alt='playlist-Artist'/>
+                    <p className='playlist-title'>{playlist.name}</p>
+                    <p className='playlist-subtitle'>{playlist.tracks.total} Songs</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  );
 }
